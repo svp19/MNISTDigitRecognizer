329e371d141c06a9f52de7ce9d10e037936cbd4b
S V Praveen <43694567+svp19@users.noreply.github.com>
2019-01-27 20:02:21 +0530

    Update digitnn.py

--- simple_neural_network/digitnn.py
+++ simple_neural_network/digitnn.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 (X_train,y_train),(X_test,y_test) = mnist.load_data() 
 '''
 #See data
-
 plt.subplot(221)
 plt.imshow(X_train[0], cmap = plt.get_cmap('gray'))
 
@@ -57,6 +56,5 @@ model.compile(optimizer='adam',loss = 'categorical_crossentropy',metrics=['accur
 model.fit(X_train,y_train, validation_data=(X_test,y_test),epochs=10,batch_size=250,verbose=2)
 
 #Final Results
-
 scores =  model.evaluate(X_test,y_test,verbose=0)
 print("Score : %.2f%%" % (scores[1]*100))
